9440121371350f2d6d5e4e50c50392f0cafb3c0a
jiyamathias <mathiasjiya2@gmail.com>
2025-11-14 16:31:07 +0100

    mod

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,14 @@
 import Navigation from "@/components/navigation"
 import Hero from "@/components/hero"
 import Services from "@/components/services"
-import RomanticDestinations from "@/components/romantic-destinations"
 import TrendingDestinations from "@/components/trending-destinations"
-import WorldTravelEasy from "@/components/world-travel-easy"
-import WhyChoose from "@/components/why-choose"
-import Footer from "@/components/footer"
 import HoneymoonSection from "@/components/horney-moon"
 import LondonStatusImage from "@/components/london-image"
 import ResortBookingSection from "@/components/resort-booking"
 import ExploreNaturePromo from "@/components/explore-nature"
 import TravelPackages from "@/components/travel-package"
 import WhyChooseZentrek from "@/components/why-choose"
+import Footer from "@/components/footer"
 
 export default function Home() {
   return (
