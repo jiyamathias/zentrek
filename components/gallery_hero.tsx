1a3bdc31088535fe6610f78380790b5ae990c9ff
jiyamathias <mathiasjiya2@gmail.com>
2025-12-28 13:07:50 +0100

    feat: gallery section worked on

---
+++ components/gallery_hero.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+const GalleryHero = () => {
+    return (
+        <section className="relative w-full h-screen flex items-center justify-center overflow-hidden">
+        <div
+            className="absolute inset-0 bg-cover bg-center"
+            style={{
+            backgroundImage: `url('/images/Rectangle 19290.png')`,
+            }}
+        >
+            <div className="absolute inset-0 bg-linear-to-b from-slate-900/40 to-slate-900/20" />
+        </div>
+
+        {/* Content */}
+        <div className="relative z-10 max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="text-center animate-fade-in-up">
+                <div className="inline-block text-white text-xl mb-1 font-bold">Explore with us</div>
+                <h1 className="text-4xl md:text-7xl font-serif italic  font-bold text-white text-balance leading-tight">
+                    Travel With Us
+                </h1>
+            </div>
+        </div>
+        </section>
+    )
+}
+
+export default GalleryHero
